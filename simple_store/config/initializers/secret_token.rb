0bc30b1315193844edb6857ab3b7dd3e869ff7f9
Damian Kadambi <damiankadambi007@hotmail.com>
2014-03-13 18:49:23 -0500

    Finished up until the the bonus section

---
+++ simple_store/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleStore::Application.config.secret_key_base = 'c58f218bf4bb0bac94a8e02f7a77314737bef1d2be5ea28b3a09fedc52eaf016bd3ed25298712b427e7ff14e3d04257211ff44269ff680ebbed087eb97cc28c9'
